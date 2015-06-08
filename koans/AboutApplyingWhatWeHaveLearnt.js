4194b6bfcf4ade11f1ba2410b5b291524719cb4f
ohsjc <ohsjeremyc@gmail.com>
2015-06-08 17:24:43 -0600

    added everything

---
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -0,0 +1,149 @@
+var _; //globals
+
+describe("About Applying What We Have Learnt", function() {
+
+  var products;
+
+  beforeEach(function () {
+    products = [
+       { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
+       { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
+       { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
+       { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
+       { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
+    ];
+  });
+
+  /*********************************************************************************/
+
+  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
+
+    var i,j,hasMushrooms, productsICanEat = [];
+
+    for (i = 0; i < products.length; i+=1) {
+        if (products[i].containsNuts === false) {
+            hasMushrooms = false;
+            for (j = 0; j < products[i].ingredients.length; j+=1) {
+               if (products[i].ingredients[j] === "mushrooms") {
+                  hasMushrooms = true;
+               }
+            }
+            if (!hasMushrooms) productsICanEat.push(products[i]);
+        }
+    }
+
+    expect(productsICanEat.length).toBe(1);
+  });
+
+  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
+
+      var productsICanEat = [];
+
+      productsICanEat = products.filter(function(x){
+        return x.containsNuts === false && x.ingredients.indexOf("mushrooms") <= 0;
+        });
+
+      expect(productsICanEat.length).toBe(1);
+  });
+
+  /*********************************************************************************/
+
+  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
+
+    var sum = 0;
+    for(var i=1; i<1000; i+=1) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+
+    expect(sum).toBe(233168);
+  });
+
+  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
+
+    Array.prototype.range = function (start, end, addBy){
+      if (arguments.length < 2){
+        start = 0;
+        end = arguments[0];
+        addBy = 1;
+      }
+      for (var x = start; x < end; x += addBy){
+        this.push(x);
+      }
+      return this;
+    }
+
+    function removeDupes(tArray){
+      for (var x = 0; x < tArray.length; x++){
+        for (var y = 0; y < tArray.length; y++){
+          if (tArray[x] === tArray[y] && x !== y)
+          tArray.splice(x,1);
+        }
+      }
+      return tArray;
+    }
+
+    var sum = [];
+    sum.range(0,1000,3);
+    sum.range(0,1000,5);
+    removeDupes(sum);
+    sum = sum.reduce(function(x,y){return x+y});
+    console.log(sum);
+
+    expect(233168).toBe(sum);
+  });
+
+  /*********************************************************************************/
+   it("should count the ingredient occurrence (imperative)", function () {
+    var ingredientCount = { "{ingredient name}": 0 };
+
+    for (i = 0; i < products.length; i+=1) {
+        for (j = 0; j < products[i].ingredients.length; j+=1) {
+            ingredientCount[products[i].ingredients[j]] = (ingredientCount[products[i].ingredients[j]] || 0) + 1;
+        }
+    }
+
+    expect(ingredientCount['mushrooms']).toBe(2);
+  });
+
+  it("should count the ingredient occurrence (functional)", function () {
+
+    var ingredientCount = {};
+
+    var theIngredients = [];
+    var theIngredients = products.map(function(x){
+        return x.ingredients;
+    });
+    theIngredients = theIngredients.reduce(function(a,b){return a.concat(b);});
+    var shroomNumb = theIngredients.filter(function(x){return x === "mushrooms";});
+    ingredientCount.mushrooms = shroomNumb.length;
+
+    expect(ingredientCount['mushrooms']).toBe(2);
+  });
+
+  /*********************************************************************************/
+  /* UNCOMMENT FOR EXTRA CREDIT */
+  /*
+  it("should find the largest prime factor of a composite number", function () {
+
+  });
+
+  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
+
+  });
+
+  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+
+
+  });
+
+  it("should find the difference between the sum of the squares and the square of the sums", function () {
+
+  });
+
+  it("should find the 10001st prime", function () {
+
+  });
+  */
+});
